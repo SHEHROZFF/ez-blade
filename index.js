4e07dc5a87b7908f41aa1a5f5fad7d64343f8552
SHEHROZFF <Muhammad.Shehroz@fissionfox.com>
2024-07-09 03:07:17 +0500

    ss

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ app.get("/auth/steam/authenticate", async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
