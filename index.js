b0eac94a21bb241c234eab60eec544798f6789d4
SHEHROZFF <Muhammad.Shehroz@fissionfox.com>
2024-07-10 02:17:07 +0500

    add stripe

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+// ---------------------------------------steam------------------------------------------------
 // Configure Passport with Steam Strategy
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -134,7 +134,48 @@ app.get("/auth/steam/return",
     res.redirect(redirectUrl);
   }
 );
+// -------------------------------------stripe-----------------------------------------
+const calculateTotalOrderAmount = (items) => {
+  if (!items || !items.length) {
+    throw new Error("Invalid items array");
+  }
+  // Multiply by 100 to convert to cents if required by Stripe
+  return items[0].amount * 100;
+};
 
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    console.log("Request received:", req.body); // Log the request body
+
+    const { items } = req.body;
+
+    if (!items || !items.length) {
+      console.error("Invalid items array");
+      return res.status(400).json({ error: "Invalid items array" });
+    }
+
+    console.log("Calculating total order amount...");
+    const amountInCents = calculateTotalOrderAmount(items);
+    console.log("Total order amount:", amountInCents);
+
+    console.log("Creating payment intent...");
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amountInCents,
+      currency: "usd",
+      description: "Payment for Gaming Levels",
+    });
+
+    console.log("Payment intent created:", paymentIntent.id);
+
+    res.json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    console.error("Error creating payment intent:", error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
+// -------------------------------------------------------------------------------------------
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
